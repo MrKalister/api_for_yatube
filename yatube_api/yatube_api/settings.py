deee691b94f730225c39ec7bb2120da068725d03
Новиков Максим <90730760+MrKalister@users.noreply.github.com>
2022-09-19 19:48:56 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,7 +1,14 @@
-from datetime import timedelta
 import os
+from datetime import timedelta
+from dotenv import load_dotenv
 
+
+load_dotenv()
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY', 'the-best-secret-key')
 
 SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
 
